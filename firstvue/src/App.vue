0144fe491f85b164427701ff96274b05683ab3ad
DESKTOP-5TBM399\lx <13343647507@163.com>
2019-05-06 16:23:57 +0800

    已更改

--- firstvue/src/App.vue
+++ firstvue/src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+   <h1>你好,我是被修改的Vue工程</h1>
   </div>
 </template>
 
@@ -12,12 +11,7 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+/* #app {
+ 
+} */
 </style>
